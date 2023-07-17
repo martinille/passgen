5b2c7b4b10249113b418a46eba740f5c15489b76
Martin Ille <ille.martin@gmail.com>
2023-07-17 23:58:00 +0200

    commit

--- assets/custom_js/general.js
+++ assets/custom_js/general.js
@@ -1,10 +1,15 @@
+"use strict";
+
+
 const copytoclipboard = () => {
 	const text = $("#password").val();
 	clipboard.copy(text);
 	$("#copy,#ok").toggle();
+	setTimeout(()=>{
+		$("#copy,#ok").toggle();
+	},3000);
 };
 
-"use strict";
 
 $(function(){
 
